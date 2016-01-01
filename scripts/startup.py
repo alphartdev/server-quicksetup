cb2eda51e59c661901048c83949d292cc926cad8
root <root@alphartdev.cloudcraft.fr>
2016-01-01 22:02:17 +0100

    Base script is setup

---
+++ scripts/startup.py
@@ -0,0 +1,38 @@
+# Simple script executed at startup
+import subprocess
+import time
+import sys
+import os
+FNULL = open(os.devnull, 'w')
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+server_name = sys.argv[1] if len(sys.argv) > 1 else "Please specify server name" 
+
+
+print bcolors.BOLD + "Welcome on " +  bcolors.OKBLUE + server_name + bcolors.ENDC + bcolors.BOLD + " server"
+
+print bcolors.UNDERLINE + "Disk usage:" + bcolors.ENDC
+print subprocess.check_output(["df", "-h"])
+
+print bcolors.UNDERLINE + "Local date: " + bcolors.ENDC + bcolors.OKBLUE + time.strftime("%c") + bcolors.ENDC + "\n"
+
+# Let's ping servers
+servers=[]
+for line in open("/etc/startupscript/hosts", 'r'):
+	print line
+	if len(line) > 0 and line[0] == "#":
+		continue
+	print line
+	servers.append((line.split(':')[0], line.split(':')[1][:-1]))
+print bcolors.UNDERLINE + "Servers state:" + bcolors.ENDC
+for server in servers:
+	print " *", server[0], "(" + server[1] + ")", "is", (bcolors.OKGREEN + "on" if subprocess.call(["ping", "-c 1", server[1]], stdout=FNULL) == 0 else bcolors.WARNING + "off"), bcolors.ENDC
---
+++ setup.sh
@@ -0,0 +1,31 @@
+# Setup file
+
+RED="\e[31m"
+BLUE="\e[94m"
+WHITE="\e[39m"
+function defprint {
+        echo -e "$BLUE""$1""$WHITE"
+}
+
+
+defprint "${RED}Starting server quick setup..."
+
+read -p "What's this server name?" name
+
+defprint "Setting up misc packages..."
+apt-get -y install python python-pip iftop
+
+defprint "Setting preferences (vi and others)"
+cp default/vimrc.local /etc/vim/vimrc.local
+
+defprint "Setting up startup script and profile"
+cp default/profile /etc/profile
+echo "python /etc/startupscript/startup.py $name" >> /etc/profile
+
+mkdir -p /etc/startupscript/
+cp scripts/startup.py /etc/startupscript/
+
+echo "# Host to be pinged at startup, format: name:ip" > /etc/startupscript/hosts
+defprint "Please configure the hosts to be pinged at login in /etc/startupscript/hosts"
+
+defprint "${RED}Installation completed!"
